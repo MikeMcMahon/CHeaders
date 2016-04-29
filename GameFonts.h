82844b9bddb542b1f050c75415e0269ce483b492
Mike McMahon <Mike McMahon>
2016-04-28 20:37:49 -0700

    Fixing the path

--- GameFonts.h
+++ GameFonts.h
@@ -1,8 +1,8 @@
 #ifdef DEBUG
 const char* GF_PRESS_START2P =
-        "D:\\Development\\dependencies\\fonts\\PressStart2P-Regular.ttf";
+        "%DEVPATH%\\dependencies\\fonts\\PressStart2P-Regular.ttf";
 const char* GF_NOVEMBER =
-        "D:\\Development\\dependencies\\fonts\\November.ttf";
+        "%DEVPATH%\\dependencies\\fonts\\November.ttf";
 #else
 const char* GF_PRESS_START2P = "PressStart2P-Regular.ttf";
 const char* GF_NOVEMBER = "November.ttf";
